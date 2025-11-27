60332121fa2abade40d53c13cdcba623db49cbea
Jay Chaudhary <82433052+JAY01-CYBER@users.noreply.github.com>
2025-11-27 11:59:00 +0530

    Create settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+// settings.gradle.kts
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Melody-M3"
+
+// Define your feature modules here
+include(":app")
+include(":ui")
+include(":domain")
+// Future module: include(":data")
