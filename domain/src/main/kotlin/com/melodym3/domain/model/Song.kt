e256ccce1eb1f4166f8407f954008828ed18f06b
Jay Chaudhary <82433052+JAY01-CYBER@users.noreply.github.com>
2025-12-03 16:12:22 +0530

    Create Song.kt

---
+++ domain/src/main/kotlin/com/melodym3/domain/model/Song.kt
@@ -0,0 +1,9 @@
+package com.melodym3.domain.model
+
+data class Song(
+    val id: String = "",
+    val title: String = "",
+    val subtitle: String = "",
+    val thumbnail: String = "",
+    val duration: String = ""
+)
