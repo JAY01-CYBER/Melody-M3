f7a8e85d8e3764282a5f31c1f58e636ffc498c23
Jay Chaudhary <82433052+JAY01-CYBER@users.noreply.github.com>
2026-03-10 15:28:38 +0530

    Update build.gradle.kts

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,14 +1,37 @@
 plugins {
-    kotlin("jvm")
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.kapt")
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+android {
+    namespace = "com.melodym3.domain"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 24
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
 
 dependencies {
-    implementation("com.google.dagger:hilt-core:2.52")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    // Hilt (For providing UseCases)
+    implementation("com.google.dagger:hilt-android:2.52")
+    kapt("com.google.dagger:hilt-compiler:2.52")
+
+    // Coroutines (Core)
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+
+    // Standard Kotlin library
+    implementation("androidx.core:core-ktx:1.13.1")
 }
