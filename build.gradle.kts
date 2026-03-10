30060613312bdce4a10155915d70e84f787baed6
Jay Chaudhary <82433052+JAY01-CYBER@users.noreply.github.com>
2026-03-10 15:58:51 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("com.android.library") version "8.3.2" apply false
     id("org.jetbrains.kotlin.android") version "2.0.0" apply false
     
-    // Compose compiler plugin for Kotlin 2.0.0
+    // Mandatory Compose compiler plugin for Kotlin 2.0.0
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" apply false
     
     id("com.google.dagger.hilt.android") version "2.52" apply false
