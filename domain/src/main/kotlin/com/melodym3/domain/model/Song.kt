8510c706dd218b630e1f0ad4b1f1263d7a865045
Jay Chaudhary <82433052+JAY01-CYBER@users.noreply.github.com>
2026-03-10 18:32:32 +0530

    Delete domain/src/main/kotlin/com/melodym3/domain/model/Song.kt

--- domain/src/main/kotlin/com/melodym3/domain/model/Song.kt
+++
@@ -1,9 +0,0 @@
-package com.melodym3.domain.model
-
-data class Song(
-    val id: String = "",
-    val title: String = "",
-    val subtitle: String = "",
-    val thumbnail: String = "",
-    val duration: String = ""
-)
