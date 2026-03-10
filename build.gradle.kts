3b4b5cfb00e983b63cd6f21e870240abe3f7a51f
Jay Chaudhary <82433052+JAY01-CYBER@users.noreply.github.com>
2026-03-10 15:04:34 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,14 @@
-plugins {
-    id("com.android.application") version "8.5.2" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.20" apply false
-    id("org.jetbrains.kotlin.jvm") version "2.0.20" apply false
-    id("com.google.dagger.hilt.android") version "2.52" apply false
-    id("com.google.gms.google-services") version "4.4.1" apply false  // Add this for Firebase
-}
+// Path: build.gradle.kts (Project Root Level)
+// Is file mein hum saare plugins ke versions define karte hain.
 
-tasks.register<Delete>("clean") {
-    delete(rootProject.buildDir)
+plugins {
+    id("com.android.application") version "8.2.2" apply false
+    id("com.android.library") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
+    
+    // Kotlin 2.0.0+ ke liye Compose compiler plugin declaration
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" apply false
+    
+    id("com.google.dagger.hilt.android") version "2.52" apply false
+    id("com.google.gms.google-services") version "4.4.1" apply false
 }
