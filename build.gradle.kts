20b51a72404766bb14fbbfc466c612c23612fda9
Jay Chaudhary <82433052+JAY01-CYBER@users.noreply.github.com>
2026-03-10 16:51:09 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,16 @@
 plugins {
+    // Android Gradle Plugin versions
     id("com.android.application") version "8.3.2" apply false
     id("com.android.library") version "8.3.2" apply false
+    
+    // Kotlin and Compose Compiler versions
     id("org.jetbrains.kotlin.android") version "2.0.0" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" apply false
-    id("com.google.dagger.hilt.android") version "2.52" apply false
-    id("com.google.gms.google-services") version "4.4.1" apply false
     
-    // KAPT definition is required here for modules to use it
+    // Dependency Injection (Hilt) and Annotation Processing (KAPT)
+    id("com.google.dagger.hilt.android") version "2.52" apply false
     id("org.jetbrains.kotlin.kapt") version "2.0.0" apply false
+    
+    // Google Services for Firebase integration
+    id("com.google.gms.google-services") version "4.4.1" apply false
 }
