5a2449e562637cad7b3058fa02fa9153021f98a2
Jay Chaudhary <82433052+JAY01-CYBER@users.noreply.github.com>
2026-03-10 16:18:07 +0530

    Update MusicModels.kt

--- data/src/main/kotlin/com/melodym3/ data/remote/model/MusicModels.kt
+++ data/src/main/kotlin/com/melodym3/ data/remote/model/MusicModels.kt
@@ -2,6 +2,10 @@ package com.melodym3.data.remote.model
 
 import com.google.gson.annotations.SerializedName
 
+/**
+ * Unified data model for MelodyM3.
+ * IMPORTANT: Delete separate files MusicItemDto.kt and StreamUrlDto.kt after creating this.
+ */
 data class MusicItemDto(
     @SerializedName("videoId") val id: String,
     @SerializedName("title") val title: String,
