886930119a150d81b9713cf00899036a64a9b364
Jay Chaudhary <82433052+JAY01-CYBER@users.noreply.github.com>
2026-03-10 09:34:35 +0530

    Update build.gradle.kts

--- ui/build.gradle.kts
+++ ui/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
     id("com.google.dagger.hilt.android")
+    id("org.jetbrains.kotlin.kapt")
 }
 
 android {
@@ -32,4 +33,5 @@ dependencies {
     
     // Hilt
     implementation("com.google.dagger:hilt-android:2.52")
+    kapt("com.google.dagger:hilt-compiler:2.52")
 }
