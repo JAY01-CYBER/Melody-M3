6ccd9453039b50743d6bc43a0125634aba80d3b5
Jay Chaudhary <82433052+JAY01-CYBER@users.noreply.github.com>
2026-03-10 13:37:36 +0530

    Update YTMService.kt

--- data/src/main/kotlin/com/melodym3/ data/remote/YTMService.kt
+++ data/src/main/kotlin/com/melodym3/ data/remote/YTMService.kt
@@ -3,13 +3,20 @@ package com.melodym3.data.remote
 import com.melodym3.data.remote.model.MusicItemDto
 import com.melodym3.data.remote.model.StreamUrlDto
 import retrofit2.http.GET
-import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface YTMService {
     
-    @GET("home")
+    @GET("search")
+    suspend fun searchSongs(
+        @Query("query") query: String
+    ): List<MusicItemDto>
+
+    @GET("stream")
+    suspend fun getStreamUrl(
+        @Query("videoId") videoId: String
+    ): StreamUrlDto
+
+    @GET("recommendations")
     suspend fun getHomeRecommendations(): List<MusicItemDto>
-    
-    @GET("stream/{videoId}")
-    suspend fun getStreamUrl(@Path("videoId") videoId: String): StreamUrlDto
 }
