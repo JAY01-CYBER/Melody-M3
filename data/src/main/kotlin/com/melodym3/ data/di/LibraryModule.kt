6f028ee4ff48739257339b29b264d89595f450ab
Jay Chaudhary <82433052+JAY01-CYBER@users.noreply.github.com>
2026-03-10 13:48:59 +0530

    Update LibraryModule.kt

--- data/src/main/kotlin/com/melodym3/ data/di/LibraryModule.kt
+++ data/src/main/kotlin/com/melodym3/ data/di/LibraryModule.kt
@@ -1,22 +1,35 @@
 package com.melodym3.data.di
 
-import com.melodym3.data.repository.FirebaseLibraryRepository
-import com.melodym3.domain.repository.LibraryRepository
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.melodym3.data.remote.YTMService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object LibraryModule {
-    
+
     @Provides
     @Singleton
-    fun provideLibraryRepository(
-        firebaseLibraryRepository: FirebaseLibraryRepository
-    ): LibraryRepository {
-        return firebaseLibraryRepository
+    fun provideFirebaseAuth(): FirebaseAuth = FirebaseAuth.getInstance()
+
+    @Provides
+    @Singleton
+    fun provideFirebaseFirestore(): FirebaseFirestore = FirebaseFirestore.getInstance()
+
+    @Provides
+    @Singleton
+    fun provideYTMService(): YTMService {
+        return Retrofit.Builder()
+            .baseUrl("https://pipedapi.kavin.rocks/") 
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(YTMService::class.java)
     }
 }
