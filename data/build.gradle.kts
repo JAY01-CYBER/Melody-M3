9dff00d37fd254c95f8da0eb2fca0bae3df35b48
Jay Chaudhary <82433052+JAY01-CYBER@users.noreply.github.com>
2026-03-10 11:02:32 +0530

    Update build.gradle.kts

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -26,11 +26,15 @@ android {
 dependencies {
     implementation(project(":domain"))
     
-    implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.google.android.material:material:1.12.0")
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
     
     // Hilt
     implementation("com.google.dagger:hilt-android:2.52")
     kapt("com.google.dagger:hilt-compiler:2.52")
+    
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
 }
